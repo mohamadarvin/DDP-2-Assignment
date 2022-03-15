17f6bb6d2934eb98869dc88d67c56214352697b2
Cornelita Lugita Santoso <lugitasantoso127@gmail.com>
2022-03-15 01:37:48 +0000

    Release Tugas Pemrograman 2

---
+++ assignment2/src/main/java/assignments/assignment2/Category.java
@@ -0,0 +1,13 @@
+package assignments.assignment2;
+
+// TODO
+public class Category {
+    private String name;
+    private int point;
+
+    @Override
+    public String toString() {
+        // TODO
+        return "";
+    }
+}
